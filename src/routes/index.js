b91e65288ba761d7b25c55d505bd0d9a4002f57e
Rahul Patwal <rahul.oneto11@gmail.com>
2023-07-21 13:56:08 +0530

    signup

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { createTweet, getTweet } from "../controllers/tweet-controller.js";
+import { signUp } from "../controllers/user-controller.js";
 
 const router = express.Router();
 
 router.post('/tweets', createTweet);
 router.get('/tweets/:id', getTweet);
+router.post('/signUp', signUp)
 
 
 
